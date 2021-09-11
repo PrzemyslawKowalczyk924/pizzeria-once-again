8d31e87ea308c846888fbbe33643256e0dc60c87
unknown <kowalczyk.przemek@wp.pl>
2021-09-11 23:50:29 +0200

    Add redirecting to #order & #booking from homePage

--- src/js/app.js
+++ src/js/app.js
@@ -9,6 +9,7 @@ const app = {
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.homeLinks = document.querySelectorAll('.link-wrapper a');
     
     const idFromHash = window.location.hash.replace('#/', '');
     
@@ -22,6 +23,18 @@ const app = {
     }
     
     thisApp.activatePage(pageMatchingHash);
+
+    for(let homeLink of thisApp.homeLinks) {
+      homeLink.addEventListener('click', function(event){
+        event.preventDefault();
+        const clickedElement = this;
+
+        const pageId = clickedElement.getAttribute('href').replace('#', '');
+        thisApp.activatePage(pageId);
+        homeLink.classList.add('active');
+        console.log('page listener', thisApp.activatePage);
+      });
+    }
 
     for(let link of thisApp.navLinks){
       link.addEventListener('click', function(event){
